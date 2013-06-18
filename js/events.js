34ec34c1c9f882af1b846fe487557b8fae1f8a23
Dave D <php@crawler.com>
2013-06-18 20:07:25 +0000

    Added username confirmation using jQuery msgbox plugin

--- js/events.js
+++ js/events.js
@@ -5,17 +5,27 @@ $(function() {
   });
 
   $('#boxclose').click(function(){ // Overlay box close
-      $('#box').animate({'top':'-200px'},500,function(){
-          $('#overlay').fadeOut('fast');
+      if(currentUser != "") {
+        $('#box').animate({'top':'-200px'},500,function(){ $('#overlay').fadeOut('fast'); });
+      } else {
+        $.msgBox({
+          title:  'No Username Chosen',
+          content:'You must select a username first!'
+        });
+      }
+  });
+
+  $('#username').click(function() { // Overlay user selectection
+    currentUser = this.value;
+    if(currentUser != "") {
+      $('#chosen-name').text(currentUser);
+      $.msgBox({
+        type   : 'info',  
+        title  : 'Username chosen',
+        content: 'Hello ' + currentUser + ' !'
       });
+    }
+    $('#boxclose').click();
   });
   
-  $('#username').click(function() { // Overlay user selectection
-    currentUser = this.value;    
-    $.msgBox({
-      title:  'Username chosen',
-      content:'Hello World! ' + currentUser
-    });
-
-  });
 });
